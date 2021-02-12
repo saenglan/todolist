0572223c50e93ef135b51873055f6b0b62b05b67
Sam <samuel.england1@gmail.com>
2021-02-12 06:22:39 -0500

    Add work and styles

--- app.js
+++ app.js
@@ -3,50 +3,55 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  let today = new Date();
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-      default:
-      console.log("Error: current day is equal to: " + currentDay);
-  }
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-  // if (currentDay === 6 || currentDay === 0) {
-  //   day = "Weekend";
-  // } else {
-  //   day = "Weekday";
-  // }
+  let day = today.toLocaleDateString("en-US", options);
+
   res.render("list", {
-    kindOfDay: day
+    listTitle: day,
+    newListItems: items
   });
 });
 
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");

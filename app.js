1a0263db0137e5ec377f00bbe470086288ee0e6c
Sam England <77686863+sam-eng11@users.noreply.github.com>
2021-02-13 05:47:02 -0500

    Add Date Module

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+console.log(date);
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -15,15 +18,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+const day = date.getDate();
 
   res.render("list", {
     listTitle: day,
@@ -32,7 +27,7 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if (req.body.list === "Work") {
     workItems.push(item);
@@ -48,7 +43,7 @@ app.get("/work", function(req, res){
 });
 
 app.post("/work", function(req, res){
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
